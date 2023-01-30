12e09e4d7ff5eac446971b666d4387560e7a5fc2
Igor Crnugelj <igor.crnugelj@d3vlab.com>
2023-01-30 12:28:26 +0100

    wallet menu item created

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -20,6 +20,9 @@ const Header = () => {
         <Link className="header-menu-item" to={"#"}>
           Pricing
         </Link>
+        <Link className="header-menu-item" to={"/wallet"}>
+          Wallet
+        </Link>
       </div>
     </div>
   );
