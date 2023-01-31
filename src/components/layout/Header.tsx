8355218201be531cc07173b55b3f69635f43ba0a
Igor Crnugelj <igor.crnugelj@d3vlab.com>
2023-01-31 09:42:01 +0100

    wallet wip

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { getEmitHelpers } from "typescript";
 import "../../css/layout/header.scss";
 
 const Header = () => {
+  const [walletAddress, setWalletAddress] = useState("");
+
+  async function requestAccount() {
+    if (window.ethereum) {
+      console.log("deteched");
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        console.log(accounts);
+        setWalletAddress(accounts[0]);
+      } catch (error) {
+        console.log("Error connecting...");
+      }
+    } else {
+      console.log("MetaMask not deteched");
+    }
+  }
+
+  async function connectWallet() {
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+    }
+  }
+
   return (
     <div className="header-container">
       <p className="header-logo">
@@ -20,7 +46,11 @@ const Header = () => {
         <Link className="header-menu-item" to={"#"}>
           Pricing
         </Link>
-        <Link className="header-menu-item" to={"/wallet"}>
+        <Link
+          className="header-menu-item"
+          to={"/wallet"}
+          onClick={requestAccount}
+        >
           Wallet
         </Link>
       </div>
