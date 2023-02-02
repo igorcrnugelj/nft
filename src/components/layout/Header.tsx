8325c569199ccf8d6e4d31fb94d34684cb6baa67
Igor Crnugelj <igor.crnugelj@d3vlab.com>
2023-02-02 17:32:26 +0100

    MetaMask integration wip

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,34 +1,7 @@
-import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { getEmitHelpers } from "typescript";
 import "../../css/layout/header.scss";
 
 const Header = () => {
-  const [walletAddress, setWalletAddress] = useState("");
-
-  async function requestAccount() {
-    if (window.ethereum) {
-      console.log("deteched");
-      try {
-        const accounts = await window.ethereum.request({
-          method: "eth_requestAccounts",
-        });
-        console.log(accounts);
-        setWalletAddress(accounts[0]);
-      } catch (error) {
-        console.log("Error connecting...");
-      }
-    } else {
-      console.log("MetaMask not deteched");
-    }
-  }
-
-  async function connectWallet() {
-    if (typeof window.ethereum !== "undefined") {
-      await requestAccount();
-    }
-  }
-
   return (
     <div className="header-container">
       <p className="header-logo">
@@ -46,12 +19,8 @@ const Header = () => {
         <Link className="header-menu-item" to={"#"}>
           Pricing
         </Link>
-        <Link
-          className="header-menu-item"
-          to={"/wallet"}
-          onClick={requestAccount}
-        >
-          Wallet
+        <Link className="header-menu-item" to={"/wallet"}>
+          Payment
         </Link>
       </div>
     </div>
--- src/components/wallet/Wallet.tsx
+++ src/components/wallet/Wallet.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { ethers } from "ethers";
+import { Fragment } from "ethers/lib/utils";
+import React, { useState } from "react";
 
 const Wallet = () => {
-  return <></>;
+  const [walletAddress, setWalletAddress] = useState("");
+
+  async function requestAccount() {
+    if (window.ethereum) {
+      console.log("deteched");
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setWalletAddress(accounts[0]);
+      } catch (error) {
+        console.log("Error connecting...");
+      }
+    } else {
+      console.log("MetaMask not deteched");
+    }
+  }
+
+  async function connectWallet() {
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+    }
+  }
+
+  return (
+    <div>
+      {/* <button onClick={requestAccount}>Request Account</button> */}
+      <button className="connect-wallet-button" onClick={connectWallet}>
+        Connect Wallet
+      </button>
+      <h3>Wallet Address: {walletAddress}</h3>
+    </div>
+  );
 };
 
 export default Wallet;
